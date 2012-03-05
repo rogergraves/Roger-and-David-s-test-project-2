1e3ffd23dfb5c6592ccb3d362928a44ac8326efd
Roger Graves <roger@rubyriders.com>
2012-03-05 06:21:39 -0800

    scanning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 RogerAndDavidSTestProject2::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
-  match '/signup', to: 'users#new'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/home', to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
